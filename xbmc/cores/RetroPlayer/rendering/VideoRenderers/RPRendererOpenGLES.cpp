a40b58d4df6f4de1e21bd1d700abd4a95c23e285
Lukas Rusak <lorusak@gmail.com>
2018-02-15 13:26:48 -0800

    RPRendererOpenGLES: allow using CRenderBufferFBO if present

--- xbmc/cores/RetroPlayer/rendering/VideoRenderers/RPRendererOpenGLES.cpp
+++ xbmc/cores/RetroPlayer/rendering/VideoRenderers/RPRendererOpenGLES.cpp
@@ -8,7 +8,9 @@
 
 #include "RPRendererOpenGLES.h"
 
+#include "cores/RetroPlayer/buffers/RenderBufferFBO.h"
 #include "cores/RetroPlayer/buffers/RenderBufferOpenGLES.h"
+#include "cores/RetroPlayer/buffers/RenderBufferPoolFBO.h"
 #include "cores/RetroPlayer/buffers/RenderBufferPoolOpenGLES.h"
 #include "cores/RetroPlayer/rendering/RenderContext.h"
 #include "cores/RetroPlayer/rendering/RenderVideoSettings.h"
@@ -38,7 +40,7 @@ CRPBaseRenderer* CRendererFactoryOpenGLES::CreateRenderer(
 
 RenderBufferPoolVector CRendererFactoryOpenGLES::CreateBufferPools(CRenderContext& context)
 {
-  return {std::make_shared<CRenderBufferPoolOpenGLES>(context)};
+  return { std::make_shared<CRenderBufferPoolFBO>(context) };
 }
 
 // --- CRPRendererOpenGLES -----------------------------------------------------
@@ -231,7 +233,7 @@ void CRPRendererOpenGLES::DrawBlackBars()
 
 void CRPRendererOpenGLES::Render(uint8_t alpha)
 {
-  CRenderBufferOpenGLES* renderBuffer = static_cast<CRenderBufferOpenGLES*>(m_renderBuffer);
+  CRenderBufferFBO *renderBuffer = static_cast<CRenderBufferFBO*>(m_renderBuffer);
 
   if (renderBuffer == nullptr)
     return;
