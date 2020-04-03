cb71b651caed55604aaf76a4eb441c52f51b80f8
Lukas Rusak <lorusak@gmail.com>
2020-04-03 11:23:35 -0700

    [settings] add settings to disable drm prime hw acceleration

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecDRMPRIME.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecDRMPRIME.cpp
@@ -30,6 +30,13 @@ extern "C"
 #include <libavutil/pixdesc.h>
 }
 
+namespace
+{
+
+constexpr const char* SETTING_VIDEOPLAYER_USEPRIMEDECODERFORHW{"videoplayer.useprimedecoderforhw"};
+
+} // namespace
+
 CDVDVideoCodecDRMPRIME::CDVDVideoCodecDRMPRIME(CProcessInfo& processInfo)
   : CDVDVideoCodec(processInfo)
 {
@@ -62,7 +69,10 @@ void CDVDVideoCodecDRMPRIME::Register()
 
 static bool IsSupportedHwFormat(const enum AVPixelFormat fmt)
 {
-  return fmt == AV_PIX_FMT_DRM_PRIME || fmt == AV_PIX_FMT_VAAPI;
+  bool hw = CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(
+      SETTING_VIDEOPLAYER_USEPRIMEDECODERFORHW);
+
+  return ((fmt == AV_PIX_FMT_DRM_PRIME || fmt == AV_PIX_FMT_VAAPI) && hw);
 }
 
 static bool IsSupportedSwFormat(const enum AVPixelFormat fmt)
@@ -72,6 +82,10 @@ static bool IsSupportedSwFormat(const enum AVPixelFormat fmt)
 
 static const AVCodecHWConfig* FindHWConfig(const AVCodec* codec)
 {
+  if (!CServiceBroker::GetSettingsComponent()->GetSettings()->GetBool(
+          SETTING_VIDEOPLAYER_USEPRIMEDECODERFORHW))
+    return nullptr;
+
   const AVCodecHWConfig* config = nullptr;
   for (int n = 0; (config = avcodec_get_hw_config(codec, n)); n++)
   {
