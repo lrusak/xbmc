5e98bc622e19dc7f18719cabdc074ffe9b965f61
Lukas Rusak <lorusak@gmail.com>
2020-10-08 11:42:59 -0700

    windowing/wayland: check for WAYLAND_DISPLAY during init

--- xbmc/windowing/wayland/WinSystemWayland.cpp
+++ xbmc/windowing/wayland/WinSystemWayland.cpp
@@ -189,6 +189,13 @@ CWinSystemWayland::~CWinSystemWayland() noexcept
 
 bool CWinSystemWayland::InitWindowSystem()
 {
+  const char* env = getenv("WAYLAND_DISPLAY");
+  if (!env)
+  {
+    CLog::Log(LOGDEBUG, "WAYLAND_DISPLAY env not set");
+    return false;
+  }
+
   wayland::set_log_handler([](std::string message)
   {
     CLog::Log(LOGWARNING, "wayland-client log message: %s", message.c_str());
