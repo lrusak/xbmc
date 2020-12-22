28415debaa27c0b5500a2432057c7d9b6ba0ede7
Lukas Rusak <lorusak@gmail.com>
2020-12-22 14:25:45 -0800

    platform/linux: register wayland/vulkan windowing

--- xbmc/platform/linux/PlatformLinux.cpp
+++ xbmc/platform/linux/PlatformLinux.cpp
@@ -36,6 +36,10 @@
 #include "windowing/gbm/WinSystemGbmGLContext.h"
 #endif
 #endif
+
+#if defined(HAS_VULKAN)
+#include "windowing/wayland/WinSystemWaylandVulkanContext.h"
+#endif
 // clang-format on
 
 #include <cstdlib>
@@ -74,6 +78,10 @@ bool CPlatformLinux::InitStageOne()
 #if defined(HAVE_GBM)
   KODI::WINDOWING::GBM::CWinSystemGbmGLContext::Register();
 #endif
+#endif
+
+#if defined(HAS_VULKAN)
+  KODI::WINDOWING::WAYLAND::CWinSystemWaylandVulkanContext::Register();
 #endif
 
   CLinuxPowerSyscall::Register();
