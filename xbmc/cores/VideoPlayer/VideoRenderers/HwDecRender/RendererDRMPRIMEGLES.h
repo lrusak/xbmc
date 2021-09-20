eaa4ccb774dd039662f2bf7765f91cf1f160a593
Lukas Rusak <lorusak@gmail.com>
2021-09-19 23:32:30 -0700

    CRendererDRMPRIMEGLES: allow selecting the tonemap method

--- xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/RendererDRMPRIMEGLES.h
+++ xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/RendererDRMPRIMEGLES.h
@@ -86,6 +86,7 @@ private:
   bool m_fullRange;
   AVColorPrimaries m_srcPrimaries;
   bool m_toneMap = false;
+  ETONEMAPMETHOD m_toneMapMethod = VS_TONEMAPMETHOD_OFF;
   bool m_passthroughHDR = false;
 
   struct BUFFER
