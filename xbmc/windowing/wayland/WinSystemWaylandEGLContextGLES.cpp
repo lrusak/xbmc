5e60a975706b19cfc032f60121310e6e3e5b3e45
Lukas Rusak <lorusak@gmail.com>
2021-10-11 10:14:24 -0700

    [temp] CWinSystemWaylandEGLContextGLES: disable DMA renderer

--- xbmc/windowing/wayland/WinSystemWaylandEGLContextGLES.cpp
+++ xbmc/windowing/wayland/WinSystemWaylandEGLContextGLES.cpp
@@ -47,7 +47,7 @@ bool CWinSystemWaylandEGLContextGLES::InitWindowSystem()
   CDVDVideoCodecDRMPRIME::Register();
   CRendererDRMPRIMEGLES::Register();
 
-  RETRO::CRPProcessInfo::RegisterRendererFactory(new RETRO::CRendererFactoryDMA);
+  // RETRO::CRPProcessInfo::RegisterRendererFactory(new RETRO::CRendererFactoryDMA);
   RETRO::CRPProcessInfo::RegisterRendererFactory(new RETRO::CRendererFactoryOpenGLES);
 
   bool general, deepColor;
