a394dd871ea3799557423b40a873fa4a414f4283
Lukas Rusak <lorusak@gmail.com>
2021-12-21 14:29:49 -0800

    MtTestUtils: use std::chrono directly

--- xbmc/test/MtTestUtils.h
+++ xbmc/test/MtTestUtils.h
@@ -13,23 +13,26 @@
 #include <chrono>
 #include <thread>
 
+using namespace std::chrono_literals;
+
 namespace ConditionPoll
 {
 /**
  * This is usually enough time for the condition to have occurred in a test.
  */
-constexpr unsigned int defaultTimeout{20000};
+constexpr auto defaultTimeout{20000ms};
 
 /**
  * poll until the lambda returns true or the timeout occurs. If the timeout occurs then
  * the function will return false. Otherwise it will return true.
  */
-template<typename L> inline bool poll(unsigned int timeoutMillis, L lambda)
+template<typename L>
+inline bool poll(std::chrono::milliseconds timeoutMillis, L lambda)
 {
-  XbmcThreads::EndTime<> endTime{std::chrono::milliseconds(timeoutMillis)};
+  XbmcThreads::EndTime<> endTime{timeoutMillis};
   bool lastValue = false;
   while (!endTime.IsTimePast() && (lastValue = lambda()) == false)
-    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    std::this_thread::sleep_for(50ms);
   return lastValue;
 }
 
