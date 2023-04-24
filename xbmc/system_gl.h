62b9267c431aaf71dfc360a9b56acef7cc39b044
Lukas Rusak <lorusak@gmail.com>
2023-04-24 10:45:33 -0700

    [HACK] try and get android to link
    Signed-off-by: Lukas Rusak <lorusak@gmail.com>

--- xbmc/system_gl.h
+++ xbmc/system_gl.h
@@ -35,6 +35,13 @@
 #endif
 
 #else
+
+#if defined(TARGET_ANDROID)
+#if !defined(GL_GLEXT_PROTOTYPES)
+#define GL_GLEXT_PROTOTYPES
+#endif
+#endif
+
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 #endif
