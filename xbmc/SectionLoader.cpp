d1499d97cd5e0365a7323f6c8fd1bef15878c88a
Lukas Rusak <lorusak@gmail.com>
2023-04-24 22:34:40 -0700

    CSectionLoader: clean up
    Signed-off-by: Lukas Rusak <lorusak@gmail.com>

--- xbmc/SectionLoader.cpp
+++ xbmc/SectionLoader.cpp
@@ -49,10 +49,10 @@ std::shared_ptr<LibraryLoader> CSectionLoader::LoadDLL(const std::string& dllnam
 
   if (dllname.empty())
     return NULL;
+
   // check if it's already loaded, and increase the reference count if so
-  for (int i = 0; i < (int)m_vecLoadedDLLs.size(); ++i)
+  for (auto& dll : m_vecLoadedDLLs)
   {
-    CDll& dll = m_vecLoadedDLLs[i];
     if (StringUtils::EqualsNoCase(dll.m_strDllName, dllname))
     {
       return dll.m_pDll;
@@ -61,15 +61,16 @@ std::shared_ptr<LibraryLoader> CSectionLoader::LoadDLL(const std::string& dllnam
 
   // ok, now load the dll
   CLog::Log(LOGDEBUG, "SECTION:LoadDLL({})", dllname);
+
   std::shared_ptr<LibraryLoader> pDll(DllLoaderContainer::LoadModule(dllname.c_str(), NULL));
   if (!pDll)
-    return NULL;
+    return {};
 
   CDll newDLL;
   newDLL.m_strDllName = dllname;
   newDLL.m_bDelayUnload=bDelayUnload;
   newDLL.m_pDll=pDll;
-  m_vecLoadedDLLs.push_back(newDLL);
+  m_vecLoadedDLLs.emplace_back(newDLL);
 
   return newDLL.m_pDll;
 }
@@ -78,29 +79,34 @@ void CSectionLoader::UnloadDLL(const std::string &dllname)
 {
   std::unique_lock<CCriticalSection> lock(m_critSection);
 
-  if (dllname.empty()) return;
-  // check if it's already loaded, and decrease the reference count if so
-  for (int i = 0; i < (int)m_vecLoadedDLLs.size(); ++i)
-  {
-    CDll& dll = m_vecLoadedDLLs[i];
-    if (StringUtils::EqualsNoCase(dll.m_strDllName, dllname))
-    {
-      if (dll.m_pDll.use_count() == 0)
-      {
-        if (dll.m_bDelayUnload)
-          dll.m_unloadDelayStartTick = std::chrono::steady_clock::now();
-        else
-        {
-          CLog::Log(LOGDEBUG, "SECTION:UnloadDll({})", dllname);
-          if (dll.m_pDll)
-            DllLoaderContainer::ReleaseModule(dll.m_pDll);
-          m_vecLoadedDLLs.erase(m_vecLoadedDLLs.begin() + i);
-        }
+  if (dllname.empty())
+    return;
 
-        return;
-      }
-    }
-  }
+  // check if it's already loaded, and decrease the reference count if so
+  m_vecLoadedDLLs.erase(std::remove_if(m_vecLoadedDLLs.begin(), m_vecLoadedDLLs.end(),
+                                       [&dllname](auto& dll)
+                                       {
+                                         if (!StringUtils::EqualsNoCase(dll.m_strDllName, dllname))
+                                           return false;
+
+                                         if (dll.m_pDll.use_count() > 1)
+                                           return false;
+
+                                         if (dll.m_bDelayUnload)
+                                           dll.m_unloadDelayStartTick =
+                                               std::chrono::steady_clock::now();
+                                         else
+                                         {
+                                           CLog::Log(LOGDEBUG, "SECTION:UnloadDll({})", dllname);
+                                           if (dll.m_pDll)
+                                             DllLoaderContainer::ReleaseModule(dll.m_pDll);
+
+                                           return true;
+                                         }
+
+                                         return false;
+                                       }),
+                        m_vecLoadedDLLs.end());
 }
 
 void CSectionLoader::UnloadDelayed()
@@ -108,34 +114,39 @@ void CSectionLoader::UnloadDelayed()
   std::unique_lock<CCriticalSection> lock(m_critSection);
 
   // check if we can unload any unreferenced dlls
-  for (int i = 0; i < (int)m_vecLoadedDLLs.size(); ++i)
-  {
-    CDll& dll = m_vecLoadedDLLs[i];
-    auto now = std::chrono::steady_clock::now();
-    auto duration =
-        std::chrono::duration_cast<std::chrono::milliseconds>(now - dll.m_unloadDelayStartTick);
-    if (dll.m_pDll.use_count() == 0 && duration > UNLOAD_DELAY)
-    {
-      CLog::Log(LOGDEBUG, "SECTION:UnloadDelayed(DLL: {})", dll.m_strDllName);
+  m_vecLoadedDLLs.erase(
+      std::remove_if(m_vecLoadedDLLs.begin(), m_vecLoadedDLLs.end(),
+                     [](auto& dll)
+                     {
+                       auto now = std::chrono::steady_clock::now();
+                       auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+                           now - dll.m_unloadDelayStartTick);
 
-      if (dll.m_pDll)
-        DllLoaderContainer::ReleaseModule(dll.m_pDll);
-      m_vecLoadedDLLs.erase(m_vecLoadedDLLs.begin() + i);
-      return;
-    }
-  }
+                       if (dll.m_pDll.use_count() == 1 && duration > UNLOAD_DELAY)
+                       {
+                         CLog::Log(LOGDEBUG, "SECTION:UnloadDelayed(DLL: {})", dll.m_strDllName);
+
+                         if (dll.m_pDll)
+                           DllLoaderContainer::ReleaseModule(dll.m_pDll);
+
+                         return true;
+                       }
+
+                       return false;
+                     }),
+      m_vecLoadedDLLs.end());
 }
 
 void CSectionLoader::UnloadAll()
 {
   // delete the dll's
   std::unique_lock<CCriticalSection> lock(m_critSection);
-  std::vector<CDll>::iterator it = m_vecLoadedDLLs.begin();
-  while (it != m_vecLoadedDLLs.end())
+
+  for (auto& dll : m_vecLoadedDLLs)
   {
-    CDll& dll = *it;
     if (dll.m_pDll)
       DllLoaderContainer::ReleaseModule(dll.m_pDll);
-    it = m_vecLoadedDLLs.erase(it);
   }
+
+  m_vecLoadedDLLs.clear();
 }
